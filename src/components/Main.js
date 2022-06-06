6280d66452bb86f3ddfec67179b7738fc0dc4020
Mohammed shahid <www.mohdshahid303@gmail.com>
2022-06-07 00:11:37 +0530

    api is placed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Main() {
+  const [country, setcountry] = useState("");
+  const [searchedcountry, setsearchedcountry] = useState("in");
+
+  const searchfunction = async () => {
+    let url = `https://restcountries.com/v2/name/${country}?fullText=true`;
+    const response = await fetch(url);
+    const ApiData = await response.json();
+    // console.log(ApiData["message"]);
+    setsearchedcountry(ApiData[0]);
+  };
+
+  const searchiongcountry = (event) => {
+    setcountry(event.target.value);
+  };
   return (
     <div className="container">
-
       <div className="row justify-content-center p-2 " id="maincontainer">
         <div className="col-md-6">
           <center>
-            <h1 style={{ textTransform  : "uppercase" }}>Country Guide  :</h1>
+            <h1 style={{ textTransform: "uppercase" }}>Country Guide :</h1>
           </center>
         </div>
         <div className="w-100 m-3"></div>
@@ -20,13 +33,14 @@ function Main() {
                 type="search"
                 placeholder="Enter City name"
                 aria-label="Search"
-                //   onChange={finidingcity}
+                onChange={searchiongcountry}
               />
 
               <button
-                className="btn btn-danger w-25" style={{borderRadius  :"35px"}}
+                className="btn btn-danger w-25"
+                style={{ borderRadius: "35px" }}
                 type="button"
-                //   onClick={searchfunction}
+                onClick={searchfunction}
               >
                 Search
               </button>
@@ -34,35 +48,67 @@ function Main() {
           </nav>
           <div className="row mt-3">
 
-            <div className="col mb-3">
-              <center><img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png?20111003033457" className="w-50" 
-              id="imageDiv" alt="" />
-              {/* <br /> */}
-              <h1>India</h1>
-              </center>
-            </div>
-            <div className="w-100"></div>
-            <div className="col" id="Details" >
-              <h3>Capital  : </h3> <span>new Delhi</span>
+            {!searchedcountry.name ? (
+              <div>
+                <center>
+                  {!searchedcountry.message?<h3> Pls Enter The City</h3>:<h3>Pls check the country nameeee properly! </h3> }
+                </center>
+                </div>
+                  
+            ) : 
+      
+            (
+              
+              <>
+                <div className="col mb-3">
+                  <center>
+                    <img
+                      src= {searchedcountry.flag}
+                      className="w-50"
+                      id="imageDiv"
+                      alt=""
+                    />
+                    {/* <br /> */}
+                    <h1>{searchedcountry.name}</h1>
+                  </center>
+                </div>
+                <div className="w-100"></div>
 
-              <br />
-              <h3>Other Names  : </h3> <span>Bharat</span>
+                <div className="col" id="Details">
+                  <h3>Capital : </h3> <span>{searchedcountry.capital}</span>
+                  <br />
+                  <h3>Other Names : </h3> <span>{searchedcountry.altSpellings}</span>
+                  <br />
+                  <h3>Demonym : </h3> <span>{searchedcountry.demonym}</span>
+                  <br />
+                  <h3>Cotinent : </h3> <span>{searchedcountry.region}</span>
+                  <br />
+                  <h3>Population : </h3> <span>{searchedcountry.population}</span>
+                  <br />
+                  <h3>Calling code : </h3> <span>+{searchedcountry.callingCodes}</span>
+                  <br />
+                  <h3>Area : </h3>{" "}
+                  <span>
+                   {searchedcountry.area/1000000} million km <sup>2</sup>
+                  </span>
+                </div>
+                <div className="w-100"></div>
 
-              <br />
-              <h3>Cotinent  : </h3> <span>Asia</span>
-              
-              <br />
-              <h3>Population  : </h3> <span>Asia</span>
-              
-              <br />
-              <h3>Calling code  : </h3> <span>Asia</span>
-              
-              <br />
-              <h3>Area  : </h3> <span>3.287 million km <sup>2</sup></span>
-              
-
-              
-            </div>
+                <div className="col" id="Details">
+                  <hr />
+                  <center><h3>Currencies</h3></center>
+                  <h3>Currency : </h3> <span>{searchedcountry.currencies[0].code}</span>
+                  <br />
+                 
+                  <h3>Name : </h3> <span>{searchedcountry.currencies[0].name}</span>
+                  <br />
+                 
+                  <h3>Symbol : </h3> <span>{searchedcountry.currencies[0].symbol}</span>
+                  <br />
+                 
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
